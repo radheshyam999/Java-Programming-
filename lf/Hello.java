56e7819bc6ca36e6c4b5506ef1b77d6328052149
Radhey Shyam <radheshyamdubey999@gmail.com>
2022-08-19 14:32:33 +0530

    added new dir  about exception handling

--- lf/Hello.java
+++ lf/Hello.java
@@ -1,8 +1,114 @@
 package lf;
+
+import java.util.Vector;
+
 /**this is simple programe for java**/
-class Hello{
+interface A{
+	int x = 0;
+ default void m1(){
 	
+	System.out.println("i m method from interface " +  this.hashCode());
+ }
+} 
+
+class Hello implements A{
+
+	//final static  int x;
+	//var y  =  0;
+	static{
+		//x =0;
+		//System.loadLibrary("C:\\Python310\\python.exe");
+	
+	}
+	
+public void m2(){
+	m1();
+}
+	@Override
+	public void m1() {
+		//System.out.println("dnkaskldnslkfdnskldfnskl");
+		//
+		if(super.hashCode() == this.hashCode()){
+			System.out.println(A.x+"we are equal" + this.hashCode());
+			//A.x = 8;
+		}else{
+			System.out.println("Base calss Object"+super.hashCode());
+			System.out.println("Current class Object:"+this.hashCode());
+		}
+		A.super.m1();
+	}
+
+	static String[] yShapedPattern(int N) {
+        // code here
+		/* 
+				if(j  == i){
+                System.out.print("* ");
+                }else{
+                    System.out.print(" ");
+                }
+            	
+				if(j == N-i){
+*/
+					
+  /*              System.out.print("* ");
+                }else{
+                    System.out.print(" ");
+                } */
+			
+
+			for(int j = 0; j <= (N); j++){
+				System.out.print("* ");
+			}
+			System.out.println();
+		
+						
+        Vector<String> v = new Vector<String>();
+        StringBuilder sb  = new StringBuilder();
+		for(int i = 0; i < N; i++){
+            sb.delete(0, sb.length());
+			for(int j = 0; j <= N; j++){
+            
+				 
+
+		if(i <= N/2){
+			System.out.print((j  == i)? "* ": " ");
+			System.out.print((j  == N-i)? "*": " ");
+
+			//sb.append((j  == i)? "* ": j);
+			//sb.append((j  == N-i)? "*": j);	
+            }else if(j == (N/2)+4){
+				System.out.print(" *");
+				//sb.append(" *");	
+				}else{
+					//sb.append("2");
+					System.out.print(" ");
+				}
+
+			    
+        }
+		v.add(sb.toString());
+		System.out.println();
+		}
+		
+		String []s = new String[v.size()];
+		for (int i = 0; i < v.size(); i++) {
+			s[i] = v.get(0);	
+		}
+    return s;    
+    }
+
+
 	public static void main(String args[]){
+		String s[] = yShapedPattern(8);
+		System.out.println("__________________________________________________");
+		
+		for (String string : s) {
+			System.out.println(string);
+		}
+		
+		
+		//new Hello().m1();
+		/** 
 		boolean b =  true;
 		boolean s = false;
 		System.out.println(s^b); 
@@ -15,6 +121,15 @@ class Hello{
 	
 	
 		System.out.println("this is simple programe for java");
+		*/
+		//System.out.println(x);
+		//var y = new Thread();
+		//System.out.println(y.currentThread().getThreadGroup().getParent());
+		
+	
+
+
+
 	}
 	
 }
---
+++ lf/Test.java
@@ -0,0 +1,67 @@
+package lf;
+
+import java.util.ArrayList;
+
+public class Test {
+
+    static void invIsoTriangle(int N) {
+       //StringBuilder sb = new StringBuilder();
+       String[]sba = new String[N];
+        char c[][] = new char[N][2*N]; 
+        // code here*
+        for(int i = 1; i <=N; i++){
+            for(int j = 1; j < 2*N; j++){
+                c[i-1][j-1] = (j >= i && j <= (2*N)-i )? '*': ' ';
+                    
+        }
+
+        //sba[i-1] = sb;  
+        System.out.println(); 
+        
+        
+        
+        
+    }
+
+
+
+
+    //char c[] = new char[N]; 
+    // code here
+    // for(int i = 1; i <=N; i++){
+    //     for(int j = 0; j < (2*N) - 1; j++){
+    //         System.out.print(c[i][j]);   
+    //         } 
+    //         System.out.println();  
+    //     }
+int l = 0;
+for(char[] sbr:c){
+    sba[l] = new String(sbr); 
+    System.out.println(sba[l++]);
+}
+
+
+
+
+
+}
+static void getMinMax(long a[], long n)  
+     {   long max = LONG.MIN_VALUE, min = LONG.MAX_VALUE;
+        //pair p = new pair();
+        //Write your code here
+        for(long  i = 0; i < n; i++){
+            if(max < a[i]) max = a[i];
+            if(min > a[i]) min = a[i];
+            
+        }
+        
+    }
+
+public static void main(String[] args) {
+    invIsoTriangle(4);
+
+    ArrayList al = new ArrayList<>();
+    al.size();
+}
+
+}
